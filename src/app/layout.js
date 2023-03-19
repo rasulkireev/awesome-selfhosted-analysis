b123c2f19f61b1f44fdf320195999101f080942f
Rasul Kireev <rasul.kireev@guycarp.com>
2023-03-19 12:19:04 -0400

    add plausible script

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,18 @@
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Awesome Self-Hosted Tools',
   description: 'Searchable list of awesome self-hosted repos',
 }
 
+
 export default function RootLayout({ children }) {
   return (
     <html class="h-full" lang="en">
+      <head>
+        <script defer data-domain="awesome-selfhosted-analysis.vercel.app" src="https://plausible.rasulkireev.com/js/script.js"></script>
+      </head>
       <body class="h-full">{children}</body>
     </html>
   )
