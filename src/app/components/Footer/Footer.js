f3f5745bb1699e7a6c05b312ff7108f4a2ed398e
Rasul Kireev <rasul.kireev@guycarp.com>
2023-03-25 11:56:41 -0400

    added footer

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 export default function Footer() {
     return (
-      <footer>
-        <a className="fixed bottom-0 left-0 z-10 px-2 py-1 font-semibold text-gray-900 no-underline bg-gray-200 border-t border-l border-white border-solid rounded-tr "
-            href="https://rasulkireev.com?ref=awesome-selfhosted-analysis"
-            target="_blank">
-            <p className="inline m-0 ml-1 text-sm font-normal align-middle">by Rasul</p>
-        </a>
+      <>
+      <a className="fixed top-0 left-0 z-10 px-2 py-1 font-semibold text-gray-900 no-underline bg-gray-200 border-b border-l border-white border-solid rounded-br "
+          href="https://rasulkireev.com?ref=awesome-selfhosted-analysis"
+          target="_blank">
+          <p className="inline m-0 ml-1 text-sm font-normal align-middle">by Rasul</p>
+      </a>
+      <footer className="bg-white">
+        <div className="p-6 mx-auto max-w-7xl md:flex md:items-center md:justify-end lg:px-8">
+          <div className="mt-8 prose prose-md md:mt-0">
+            <p className="text-lg leading-5 text-center text-gray-500">
+                By the <a className="underline" href="https://github.com/awesome-selfhosted/awesome-selfhosted">
+                  awesome-selfhosted
+                </a> community, licensed under <a className="underline" href="https://github.com/awesome-selfhosted/awesome-selfhosted/blob/master/LICENSE">
+                  CC-BY-SA-3.0
+                </a>
+            </p>
+          </div>
+        </div>
       </footer>
+      </>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -6,16 +6,16 @@ import Footer from './components/Footer'
 export default function Home() {
   return (
     <>
-      <div className="py-10">
+      <div className="pt-10">
         <header>
-      <div className="bg-white pt-24 px-6 pb-8 lg:px-8">
-        <div className="mx-auto max-w-2xl text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Awesome Self-Hosted</h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            This is my attempt to do some cool stuff with Typesense and Next.js
-          </p>
-        </div>
-      </div>
+            <div className="px-6 pt-24 pb-8 bg-white lg:px-8">
+              <div className="max-w-2xl mx-auto text-center">
+                <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Awesome Self-Hosted Search</h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  This is my attempt to do some cool stuff with Typesense and Next.js
+                </p>
+              </div>
+            </div>
         </header>
         <main>
           <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
